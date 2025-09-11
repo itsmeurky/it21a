804f3e8611e91f59b98d8330fdebffa41e4337be
mikay <michaelladrenojo@gmail.com>
2025-09-11 09:25:56 +0800

    Added person.js

---
+++ person.js
@@ -0,0 +1,28 @@
+class Person{
+   
+    constructor(name,age,occupation){
+         this.name = name;
+         this.age = age;
+         this.occupation = occupation;
+    }
+
+    getName(){
+        console.log(this.name);
+    }
+
+    
+    displayInfo(){
+        console.log("name: " + this.name);
+        console.log("age: " + this.age);
+        console.log("occupation: " + this.occupation);
+        console.log("..........");
+    }
+
+}
+
+const person1 = new Person("Coco Martin", "24" , "Masahista");
+
+person1.getName();
+person1.displayInfo();
+
+
