4103d1f3fc0be4cb000cadf7dd9de42d74682e7e
michaelladrenojo@gmail.com <michaelladrenojo@gmail.com>
2025-09-15 08:59:20 +0800

    added a casts fireball

--- hero.js
+++ hero.js
@@ -18,8 +18,18 @@ class Warrior extends Hero{
     }
 }   
 
-const warrior = new Warrior("Thorin",100,10);
-warrior.useAbility()
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+    useAbility(){
+        console.log(`${this.name}'casts Fireball`);
+    }
+}
 
-    
-    
+const warrior = new Warrior("Thorin",100,10);
+warrior.useAbility();
+
+const mage = new Mage("gandalf",80,8,50);
+mage.useAbility();
